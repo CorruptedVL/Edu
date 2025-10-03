a266d37df70cad105728c27a40612c988d4f047b
Evgeniy Chalyshev <zhenka@list.ru>
2025-10-03 13:43:45 +1000

    New Planets with Coord Math.hypot distance to Star

--- src/main/java/mg/edu/astronomy/cosmic_bodies/Planet.java
+++ src/main/java/mg/edu/astronomy/cosmic_bodies/Planet.java
@@ -2,22 +2,33 @@ package mg.edu.astronomy.cosmic_bodies;
 
 public class Planet extends AbstractBody {
 
-    private double distanceToStar;
-    private double x;
-    private double y;
+    private final double distanceToStar;
+    private final double x;
+    private final double y;
 
     public Planet(String name, double distanceToStar) {
         super(name);
         this.distanceToStar = distanceToStar;
+        this.x = Double.NaN;
+        this.y = Double.NaN;
     }
 
     public Planet(String name, double x, double y) {
         super(name);
         this.x = x;
         this.y = y;
+        this.distanceToStar = Math.hypot(x, y);
     }
 
     public double distanceToStar() {
         return distanceToStar;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
     }
 }
