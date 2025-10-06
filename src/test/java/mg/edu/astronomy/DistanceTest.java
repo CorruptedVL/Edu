d3376c3870cabbbb971e5cdd731b767726c1631a
Evgeniy Chalyshev <zhenka@list.ru>
2025-10-06 17:08:57 +1000

    new DistanceTest.java (not a test yet :)

--- src/test/java/mg/edu/astronomy/DistanceTest.java
+++ src/test/java/mg/edu/astronomy/DistanceTest.java
@@ -3,6 +3,9 @@ package mg.edu.astronomy;
 import mg.edu.astronomy.estimators.DistanceByDarkMatter;
 import mg.edu.astronomy.estimators.DistanceByOrbitRadius;
 import mg.edu.astronomy.estimators.DistanceByCoordinates;
+import mg.edu.astronomy.selector.DistanceEstimator;
+import mg.edu.astronomy.selector.DistanceStrategySelectorByRules;
+import mg.edu.astronomy.selector.StrategySelector;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -13,14 +16,29 @@ public class DistanceTest {
     DistanceByOrbitRadius distanceByOrbitRadius = new DistanceByOrbitRadius();
     DistanceByCoordinates distanceByCoordinates = new DistanceByCoordinates();
     DistanceByDarkMatter distanceByDarkMatter= new DistanceByDarkMatter();
+    StrategySelector selector = new DistanceStrategySelectorByRules();
+    DistanceEstimator estimator = new DistanceEstimator(selector);
+
 
     @Test
     public void fromMercuryToVenus() {
         var distByOrbit = distanceByOrbitRadius.estimate(solarSystem.mercury, solarSystem.venus);
+        System.out.println(distByOrbit);
+        System.out.println(estimator.estimateAll(solarSystem.mercury, solarSystem.venus));
     }
 
     @Test
+    public void fromMarsToNeptune() {
+
+        System.out.println(estimator.estimateAll(solarSystem.mars, solarSystem.neptune));
+
+    }
+
+
+
+    @Test
     public void fromEarthToMercury() {
+
     }
     @Test
     public void fromJupiterToEarth() {
