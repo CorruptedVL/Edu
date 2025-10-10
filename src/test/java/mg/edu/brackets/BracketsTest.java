821972ad7d7777d1ebc41c2c40ebd62aed6ce8ea
Evgeniy Chalyshev <zhenka@list.ru>
2025-10-10 16:12:22 +1000

    Чуть больше вариантов для теста

--- src/test/java/mg/edu/brackets/BracketsTest.java
+++ src/test/java/mg/edu/brackets/BracketsTest.java
@@ -11,10 +11,14 @@ public class BracketsTest {
         var brackets1 = new Brackets ("([{}])");
         var brackets2 = new Brackets("");
         var brackets3  = new Brackets("()");
+        var brackets4  = new Brackets("([{}])(");
+        var brackets5  = new Brackets("([{}]))");
 
         assertTrue (bracketValidator.isValid(brackets1.chars));
         assertFalse(bracketValidator.isValid(brackets2.chars));
         assertTrue(bracketValidator.isValid(brackets3.chars));
+        assertFalse(bracketValidator.isValid(brackets4.chars));
+        assertFalse(bracketValidator.isValid(brackets5.chars));
 
     }
 }
